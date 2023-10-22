c374d5bc64c95ef110a8835ac8271171ccc39aeb
franckv <franck.vdl@gmail.com>
2023-10-22 22:15:20 +0200

    Add basic shapes

--- gobs-scene/src/shape.rs
+++ gobs-scene/src/shape.rs
@@ -13,30 +13,142 @@ impl Shapes {
 
         let (top, bottom, left, right) = (0.5, -0.5, -0.5, 0.5);
 
-        let v1 = [left, bottom, 0.];
-        let v2 = [right, bottom, 0.];
-        let v3 = [(left + right) / 2., top, 0.];
+        let v = [
+            [left, bottom, 0.],
+            [right, bottom, 0.],
+            [(left + right) / 2., top, 0.],
+        ];
 
-        let n = [0., 0., 1.];
+        let n = [[0., 0., 1.]];
 
-        let t1 = [0., 1.];
-        let t2 = [1., 1.];
-        let t3 = [0.5, 0.];
+        let t = [[0., 1.], [1., 1.], [0.5, 0.]];
+
+        let vi = [1, 2, 3];
+
+        let ni = [1, 1, 1];
+
+        let ti = [1, 2, 3];
 
         match flags {
             VertexFlag::POSITION => {
-                builder = builder
-                    .add_vertex_P(v1.into())
-                    .add_vertex_P(v2.into())
-                    .add_vertex_P(v3.into())
+                for i in 0..vi.len() {
+                    builder = builder.add_vertex_P(v[vi[i] - 1].into())
+                }
             }
             VertexFlag::PTN => {
-                builder = builder
-                    .add_vertex_PTN(v1.into(), t1.into(), n.into())
-                    .add_vertex_PTN(v2.into(), t2.into(), n.into())
-                    .add_vertex_PTN(v3.into(), t3.into(), n.into())
+                for i in 0..vi.len() {
+                    builder = builder.add_vertex_PTN(
+                        v[vi[i] - 1].into(),
+                        t[ti[i] - 1].into(),
+                        n[ni[i] - 1].into(),
+                    )
+                }
             }
             _ => todo!("triangle"),
+        }
+        builder.build(gfx)
+    }
+
+    pub fn quad(gfx: &Gfx, flags: VertexFlag) -> Mesh {
+        let mut builder = MeshBuilder::new("quad", flags);
+
+        let (top, bottom, left, right) = (0.5, -0.5, -0.5, 0.5);
+
+        let v = [
+            [left, top, 0.],
+            [right, top, 0.],
+            [left, bottom, 0.],
+            [right, bottom, 0.],
+        ];
+
+        let n = [[0., 0., 1.]];
+
+        let t = [[0., 0.], [1., 0.], [0., 1.], [1., 1.]];
+
+        let vi = [1, 3, 4, 4, 2, 1];
+
+        let ni = [1, 1, 1, 1, 1, 1];
+
+        let ti = [1, 3, 4, 4, 2, 1];
+
+        match flags {
+            VertexFlag::POSITION => {
+                for i in 0..vi.len() {
+                    builder = builder.add_vertex_P(v[vi[i] - 1].into())
+                }
+            }
+            VertexFlag::PTN => {
+                for i in 0..vi.len() {
+                    builder = builder.add_vertex_PTN(
+                        v[vi[i] - 1].into(),
+                        t[ti[i] - 1].into(),
+                        n[ni[i] - 1].into(),
+                    )
+                }
+            }
+            _ => todo!("quad"),
+        }
+        builder.build(gfx)
+    }
+
+    pub fn cube(gfx: &Gfx, flags: VertexFlag) -> Mesh {
+        let mut builder = MeshBuilder::new("cube", flags);
+
+        let (top, bottom, left, right, front, back) = (0.5, -0.5, -0.5, 0.5, 0.5, -0.5);
+
+        let v = [
+            [left, top, front],
+            [right, top, front],
+            [left, bottom, front],
+            [right, bottom, front],
+            [left, top, back],
+            [right, top, back],
+            [left, bottom, back],
+            [right, bottom, back],
+        ];
+
+        let n = [
+            [0., 0., 1.],
+            [0., 0., -1.],
+            [-1., 0., 0.],
+            [1., 0., 0.],
+            [0., 1., 0.],
+            [0., -1., 0.],
+        ];
+
+        let t = [[0., 0.], [1., 0.], [0., 1.], [1., 1.]];
+
+        let vi = [
+            3, 4, 2, 3, 2, 1, 8, 7, 5, 8, 5, 6, 7, 3, 1, 7, 1, 5, 4, 8, 6, 4, 6, 2, 1, 2, 6, 1, 6,
+            5, 7, 8, 4, 7, 4, 3,
+        ];
+
+        let ni = [
+            1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5,
+            5, 6, 6, 6, 6, 6, 6,
+        ];
+
+        let ti = [
+            3, 4, 2, 3, 2, 1, 3, 4, 2, 3, 2, 1, 3, 4, 2, 3, 2, 1, 3, 4, 2, 3, 2, 1, 3, 4, 2, 3, 2,
+            1, 3, 4, 2, 3, 2, 1,
+        ];
+
+        match flags {
+            VertexFlag::POSITION => {
+                for i in 0..vi.len() {
+                    builder = builder.add_vertex_P(v[vi[i] - 1].into())
+                }
+            }
+            VertexFlag::PTN => {
+                for i in 0..vi.len() {
+                    builder = builder.add_vertex_PTN(
+                        v[vi[i] - 1].into(),
+                        t[ti[i] - 1].into(),
+                        n[ni[i] - 1].into(),
+                    )
+                }
+            }
+            _ => todo!("cube"),
         }
         builder.build(gfx)
     }
