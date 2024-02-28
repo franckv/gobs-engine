3c512d687acd1416e86d22a745b1a88f01da12d0
franckv <franck.vdl@gmail.com>
2024-02-28 12:52:40 +0100

    Refactor gltf loading

--- gobs-assets/src/gltf.rs
+++ gobs-assets/src/gltf.rs
@@ -1,12 +1,16 @@
 use std::{path::Path, sync::Arc};
 
-use gltf::mesh::util::{ReadColors, ReadIndices};
+use gltf::{
+    buffer, image,
+    mesh::util::{ReadColors, ReadIndices},
+    Document,
+};
 
 use gobs_core::Color;
 use gobs_render::{
     context::Context,
     geometry::{Mesh, Model, VertexData, VertexFlag},
-    material::{Material, MaterialProperty, Texture, TextureType},
+    material::{Material, MaterialInstance, MaterialProperty, Texture, TextureType},
     pass::RenderPass,
     ImageExtent2D, SamplerFilter,
 };
@@ -17,101 +21,22 @@ where
 {
     let (doc, buffers, images) = gltf::import(file).unwrap();
 
+    let textures = load_textures(ctx, &doc, &images);
+    let default_texture = textures.last().unwrap().clone();
+
+    let materials = load_materials(ctx, pass.clone(), &doc, &textures, default_texture);
+    let default_material_instance = materials.last().unwrap().clone();
+
+    load_models(&doc, &buffers, default_material_instance, &materials)
+}
+
+fn load_models(
+    doc: &Document,
+    buffers: &[buffer::Data],
+    default_material_instance: Arc<MaterialInstance>,
+    materials: &[Arc<MaterialInstance>],
+) -> Vec<Arc<Model>> {
     let mut models = Vec::new();
-
-    let vertex_flags = VertexFlag::POSITION
-        | VertexFlag::TEXTURE
-        | VertexFlag::NORMAL
-        | VertexFlag::TANGENT
-        | VertexFlag::BITANGENT;
-
-    let texture_material = Material::builder("gltf.texture.vert.spv", "gltf.texture.frag.spv")
-        .vertex_flags(vertex_flags)
-        .prop("diffuse", MaterialProperty::Texture)
-        .build(ctx, pass.clone());
-
-    let vertex_flags = VertexFlag::POSITION | VertexFlag::COLOR;
-
-    let color_material = Material::builder("gltf.color.vert.spv", "gltf.color.frag.spv")
-        .vertex_flags(vertex_flags)
-        .build(ctx, pass);
-
-    let default_texture = Texture::default(ctx);
-    let default_material_instance = texture_material
-        .clone()
-        .instantiate(vec![default_texture.clone()]);
-    log::debug!("Default material id: {}", default_material_instance.id);
-
-    let color_material_instance = color_material.instantiate(vec![]);
-    log::debug!("Color material id: {}", color_material_instance.id);
-
-    let mut materials = vec![];
-    let mut textures = vec![];
-
-    log::info!("Reading {} images", images.len());
-
-    for t in doc.textures() {
-        let name = t.name().unwrap_or_default();
-        let image = t.source();
-        let data = &images[image.index()];
-
-        log::info!(
-            "Texture #{}: {}, image #{}, format: {:?}",
-            t.index(),
-            name,
-            image.index(),
-            data.format
-        );
-
-        match data.format {
-            gltf::image::Format::R8G8B8A8 => {
-                let texture = Texture::new(
-                    ctx,
-                    name,
-                    &data.pixels,
-                    ImageExtent2D::new(data.width, data.height),
-                    TextureType::Diffuse,
-                    SamplerFilter::FilterLinear,
-                );
-
-                textures.push(texture);
-            }
-            _ => {
-                textures.push(default_texture.clone());
-                log::warn!("Unsupported image format: {:?}", data.format)
-            }
-        };
-    }
-
-    for mat in doc.materials() {
-        let name = mat.name().unwrap_or_default();
-        if let Some(idx) = mat.index() {
-            log::info!("Material #{}: {}", idx, name);
-
-            let pbr = mat.pbr_metallic_roughness();
-            let diffuse = pbr.base_color_texture();
-
-            match diffuse {
-                Some(tex_info) => {
-                    log::info!(
-                        "Using texture #{}: {}",
-                        tex_info.texture().index(),
-                        tex_info.texture().name().unwrap_or_default()
-                    );
-                    let texture = textures[tex_info.texture().index()].clone();
-                    let material_instance = texture_material.instantiate(vec![texture]);
-                    materials.push(material_instance.clone());
-                }
-                None => {
-                    let color: Color = pbr.base_color_factor().into();
-                    log::info!("Using color material: {:?}", color);
-                    materials.push(color_material_instance.clone());
-                }
-            }
-        } else {
-            log::info!("Using default material");
-        }
-    }
 
     for m in doc.meshes() {
         let name = m.name().unwrap_or_default();
@@ -205,4 +130,118 @@ where
     }
 
     models
+}
+
+fn load_materials(
+    ctx: &Context,
+    pass: Arc<dyn RenderPass>,
+    doc: &Document,
+    textures: &[Texture],
+    default_texture: Texture,
+) -> Vec<Arc<MaterialInstance>> {
+    let mut materials = vec![];
+
+    let vertex_flags = VertexFlag::POSITION
+        | VertexFlag::TEXTURE
+        | VertexFlag::NORMAL
+        | VertexFlag::TANGENT
+        | VertexFlag::BITANGENT;
+
+    let texture_material = Material::builder("gltf.texture.vert.spv", "gltf.texture.frag.spv")
+        .vertex_flags(vertex_flags)
+        .prop("diffuse", MaterialProperty::Texture)
+        .build(ctx, pass.clone());
+
+    let vertex_flags = VertexFlag::POSITION | VertexFlag::COLOR;
+
+    let color_material = Material::builder("gltf.color.vert.spv", "gltf.color.frag.spv")
+        .vertex_flags(vertex_flags)
+        .build(ctx, pass.clone());
+
+    let default_material_instance = texture_material
+        .clone()
+        .instantiate(vec![default_texture.clone()]);
+    log::debug!("Default material id: {}", default_material_instance.id);
+
+    let color_material_instance = color_material.instantiate(vec![]);
+    log::debug!("Color material id: {}", color_material_instance.id);
+
+    for mat in doc.materials() {
+        let name = mat.name().unwrap_or_default();
+        if let Some(idx) = mat.index() {
+            log::info!("Material #{}: {}", idx, name);
+
+            let pbr = mat.pbr_metallic_roughness();
+            let diffuse = pbr.base_color_texture();
+
+            match diffuse {
+                Some(tex_info) => {
+                    log::info!(
+                        "Using texture #{}: {}",
+                        tex_info.texture().index(),
+                        tex_info.texture().name().unwrap_or_default()
+                    );
+                    let texture = textures[tex_info.texture().index()].clone();
+                    let material_instance = texture_material.instantiate(vec![texture]);
+                    materials.push(material_instance.clone());
+                }
+                None => {
+                    let color: Color = pbr.base_color_factor().into();
+                    log::info!("Using color material: {:?}", color);
+                    materials.push(color_material_instance.clone());
+                }
+            }
+        } else {
+            log::info!("Using default material");
+        }
+    }
+
+    materials.push(default_material_instance);
+
+    materials
+}
+
+fn load_textures(ctx: &Context, doc: &Document, images: &[image::Data]) -> Vec<Texture> {
+    log::info!("Reading {} images", images.len());
+
+    let mut textures = vec![];
+
+    let default_texture = Texture::default(ctx);
+
+    for t in doc.textures() {
+        let name = t.name().unwrap_or_default();
+        let image = t.source();
+        let data = &images[image.index()];
+
+        log::info!(
+            "Texture #{}: {}, image #{}, format: {:?}",
+            t.index(),
+            name,
+            image.index(),
+            data.format
+        );
+
+        match data.format {
+            gltf::image::Format::R8G8B8A8 => {
+                let texture = Texture::new(
+                    ctx,
+                    name,
+                    &data.pixels,
+                    ImageExtent2D::new(data.width, data.height),
+                    TextureType::Diffuse,
+                    SamplerFilter::FilterLinear,
+                );
+
+                textures.push(texture);
+            }
+            _ => {
+                textures.push(default_texture.clone());
+                log::warn!("Unsupported image format: {:?}", data.format)
+            }
+        };
+    }
+
+    textures.push(default_texture);
+
+    textures
 }
