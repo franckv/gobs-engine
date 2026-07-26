6d717ad86470281cbeb254cb90725f0e82c1f35b
franckv <franck.vdl@gmail.com>
2026-07-26 16:30:09 +0200

    Convert UI shaders to slang

--- gobs-assets/build.rs
+++ gobs-assets/build.rs
@@ -1,6 +1,5 @@
-use gobs_build::{compile_glsl_shaders, compile_slang_shaders, copy_files};
+use gobs_build::{compile_slang_shaders, copy_files};
 
-const SHADERS_GLSL_DIR: &str = "shaders/glsl";
 const SHADERS_SLANG_DIR: &str = "shaders/slang";
 const SHADERS_SLANG_INCLUDE_DIR: &str = "shaders/slang/include";
 const SHADERS_OUT_DIR: &str = "shaders/spv";
@@ -9,13 +8,10 @@ const SHADERS_DIR: &str = "shaders";
 const RESOURCES_DIR: &str = "resources";
 
 fn main() {
-    println!("cargo:rerun-if-changed={SHADERS_GLSL_DIR}/");
     println!("cargo:rerun-if-changed={SHADERS_SLANG_DIR}/");
     println!("cargo:rerun-if-changed={SHADERS_SLANG_INCLUDE_DIR}/");
     println!("cargo:rerun-if-changed={RESOURCES_DIR}/");
 
-    compile_glsl_shaders(SHADERS_GLSL_DIR, SHADERS_OUT_DIR, SHADERS_ASM_DIR)
-        .expect("Compile shaders");
     compile_slang_shaders(
         SHADERS_SLANG_DIR,
         SHADERS_SLANG_INCLUDE_DIR,
