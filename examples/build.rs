8e62dc9625c7081b774181cb9621acc62b02fbfa
franckv <franck.vdl@gmail.com>
2026-07-26 17:05:38 +0200

    Port compute shader to slang

--- examples/build.rs
+++ examples/build.rs
@@ -1,6 +1,5 @@
-use gobs_build::{compile_glsl_shaders, compile_slang_shaders, copy_files};
+use gobs_build::{compile_slang_shaders, copy_files};
 
-const SHADERS_GLSL_DIR: &str = "shaders/glsl";
 const SHADERS_SLANG_DIR: &str = "shaders/slang";
 const SHADERS_SLANG_INCLUDE_DIR: &str = "../gobs-assets/shaders/slang/include";
 const SHADERS_OUT_DIR: &str = "shaders/spv";
@@ -10,14 +9,11 @@ const RESOURCES_DIR: &str = "resources";
 const SHADERS_DIR: &str = "shaders";
 
 fn main() {
-    println!("cargo:rerun-if-changed={SHADERS_GLSL_DIR}/");
     println!("cargo:rerun-if-changed={SHADERS_SLANG_DIR}/");
     println!("cargo:rerun-if-changed={SHADERS_SLANG_INCLUDE_DIR}/");
     println!("cargo:rerun-if-changed={ASSETS_DIR}/");
     println!("cargo:rerun-if-changed={RESOURCES_DIR}/");
 
-    compile_glsl_shaders(SHADERS_GLSL_DIR, SHADERS_OUT_DIR, SHADERS_ASM_DIR)
-        .expect("Compile shaders");
     compile_slang_shaders(
         SHADERS_SLANG_DIR,
         SHADERS_SLANG_INCLUDE_DIR,
